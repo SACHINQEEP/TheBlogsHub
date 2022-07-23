4a1e5d379335b1eefc2d648f7087b527777c92cd
SACHINQEEP <patelsachinsp269@gmail.com>
2022-07-23 15:54:31 +0530

    some small changes

--- config/config.js
+++ config/config.js
@@ -4,9 +4,9 @@ const CONFIG = {}
 
 CONFIG.app = process.env.APP || 'dev'
 
-CONFIG.port = process.env.PORT || 7000
+CONFIG.port = process.env.PORT || 15869
 
-CONFIG.database = process.env.DATABASE || 'blogs'
+CONFIG.database = process.env.DATABASE || "blogsHub"
 
 CONFIG.Secrat_key = process.env.SECRAT_KEY 
 
