4a3f6bc5f2244cdff50b2e93e57bb3dd8b5519ec
SACHINQEEP <patelsachinsp269@gmail.com>
2022-07-25 22:22:36 +0530

    made some changes

--- index.js
+++ index.js
@@ -5,18 +5,6 @@ const db = require('./models')
 const CONFIG = require('./config/config');
 const userRouter = require("./APIs/routers/userRouter")
 
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: "postgres://fbpzrigslyjxdp:2f7e7d0cbc355ed4ef73f269261cb665967563f5c72c14b1d7c79d8859a9b23f@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d3m3r5oqg5uop2",
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-
-
-
-
 let app = express()
 
 app.use(express.json())
@@ -29,22 +17,6 @@ app.use(
 )
 
 app.use("/v1/api", userRouter);
-app.get("/", async (req, res)=> {
-  console.log("hello heroku")
-})
-
-// router.get('/db', async (req, res) => {
-//   try {
-//     const client = await pool.connect();
-//     const result = await client.query('SELECT * FROM test_table');
-//     const results = { 'results': (result) ? result.rows : null};
-//     res.render('pages/db', results );
-//     client.release();
-//   } catch (err) {
-//     console.error(err);
-//     res.send("Error " + err);
-//   }
-// })
 
 
 db.sequelize
