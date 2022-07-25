113f5d7c033238f66d3f09a5b8c2805864232496
SACHINQEEP <patelsachinsp269@gmail.com>
2022-07-25 21:49:29 +0530

    made some changes

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const db = require('./models')
 const CONFIG = require('./config/config');
 const userRouter = require("./APIs/routers/userRouter")
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: "postgres://fbpzrigslyjxdp:2f7e7d0cbc355ed4ef73f269261cb665967563f5c72c14b1d7c79d8859a9b23f@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d3m3r5oqg5uop2",
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: "postgres://fbpzrigslyjxdp:2f7e7d0cbc355ed4ef73f269261cb665967563f5c72c14b1d7c79d8859a9b23f@ec2-34-247-72-29.eu-west-1.compute.amazonaws.com:5432/d3m3r5oqg5uop2",
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
 
 
 
 
 let app = express()
-
-let router = express.Router();
 
 app.use(express.json())
 app.use(
@@ -31,19 +29,22 @@ app.use(
 )
 
 app.use("/v1/api", userRouter);
-
-router.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
+app.get("/", async (req, res)=> {
+  console.log("hello heroku")
 })
+
+// router.get('/db', async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query('SELECT * FROM test_table');
+//     const results = { 'results': (result) ? result.rows : null};
+//     res.render('pages/db', results );
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// })
 
 
 db.sequelize
@@ -62,7 +63,7 @@ db.sequelize
     )
   })
 
-let port = CONFIG.port
+let port = process.env.PORT || 8000
 
 app.listen(port, () => {
   console.log(`Server Listed on ${port}`)
